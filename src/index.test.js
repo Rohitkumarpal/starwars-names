8bf45f125f17bf2f9b1b19243b0d6aae20d41341
Rohitkumarpal <Rohit_kumar_pal@discovery.com>
2022-05-16 13:22:39 +0530

    added travis.yaml and converted testfile import into es6

--- src/index.test.js
+++ src/index.test.js
@@ -1,5 +1,5 @@
-var expect = require('chai').expect;
-var  starWars = require('./index')
+import {expect} from 'chai';
+import starWars from './index';
 
 describe('starwars-names',()=>  {
     describe('all',()=> {
@@ -26,6 +26,14 @@ describe('starwars-names',()=>  {
         it('should return random item from the starWars.all ',()=>{
             var randomItem = starWars.random();
             expect(starWars.all).to.include(randomItem);
+        })
+
+        it('should return an array of random items if passed a number ', ()=>{
+            var randomItems = starWars.random(3);
+            expect(randomItems).to.have.length(3);
+            randomItems.forEach((item) => {
+                expect(starWars.all).to.include(item);
+            });
         })
     })
    
